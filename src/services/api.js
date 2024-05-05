103651cdb5ab07d90c467dc68f69fb99be378691
Gungde Iswara <86350332+gungde120@users.noreply.github.com>
2024-05-06 01:11:44 +0800

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,5 @@
 // const API_URL = "https://api.kpsdps.com.kpsdenpasar.com";
 // const API_URL = "http://localhost:8081";
-const API_URL = "kps-backend.vercel.app";
+const API_URL = "https://kps-backend.vercel.app";
 
 export default API_URL;
